201d6ce6263ffaff86b0a3cebecbd2b5351d2491
F0sya <nikitachmyria@gmail.com>
2024-05-16 18:57:21 +0300

    First practice exercise is done

--- cpp_cycles_part3/main.cpp
+++ cpp_cycles_part3/main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+	int left_limit; cout << "Enter a left limit:"; cin >> left_limit;
+	int right_limit; cout << "Enter a right limit:"; cin >> right_limit;
+	int guess; cout << "Enter your guess: "; cin >> guess;
+
+	while (guess != true) {
+		for (int i = left_limit; i <= right_limit; i++) {
+			if (i == guess) {
+				cout << "Good job!" << endl;
+				guess = true;
+				return 0;
+			}
+		}
+		cout << "Wrong!\n";
+		cout << "Enter your guess: ";  cin >> guess;
+	}
 
 }
